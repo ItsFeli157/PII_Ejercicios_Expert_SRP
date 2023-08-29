6f7c7b8e332c1cd4432a8bc7fd706fe4674c19f2
Felipe <felipelarumbe157@gmail.com>
2023-08-29 20:49:35 -0300

    cambios con SRP

--- Ejercicio 1/src/Library/Book.cs
+++ Ejercicio 1/src/Library/Book.cs
@@ -8,8 +8,8 @@ namespace SRP
         public string Title { get ; }
         public string Author { get ; }
         public string Code { get ;  }
-        public string LibrarySector { get ; set; }
-        public string LibraryShelve { get ; set; }
+        //public string LibrarySector { get ; set; }
+        //public string LibraryShelve { get ; set; }
 
         public Book(String title, String author, String code)
         {
@@ -17,12 +17,42 @@ namespace SRP
             this.Author = author;
             this.Code = code;
         }
+    }
+        /*public void ShelveBook(String sector, String shelve)
+        {
+            this.LibrarySector = sector;
+            this.LibraryShelve = shelve;
+        }*/
 
-        public void ShelveBook(String sector, String shelve)
+    public class Sector
+    {
+        public string Name {get; }
+
+        public Sector(string name)
+        {
+            this.Name = name;
+        }
+    }
+
+    public class Shelve
+    {
+        public string Number {get; }
+        
+        public Shelve(string number)
+        {
+            this.Number = number;
+        }
+    }
+
+    public class LibraryBook
+    {
+        public Sector LibrarySector {get; set;}
+        public Shelve LibraryShelve {get; set;}
+
+        public void ShelveBook(Sector sector, Shelve shelve)
         {
             this.LibrarySector = sector;
             this.LibraryShelve = shelve;
         }
-
     }
 }
--- Ejercicio 1/src/Program/Program.cs
+++ Ejercicio 1/src/Program/Program.cs
@@ -9,9 +9,25 @@ namespace SRP
         {
             Book book1 = new Book("Design Patterns","Erich Gamma & Others","001-034");
             Book book2 = new Book("Pro C#","Troelsen","001-035");
-            book1.ShelveBook("A","7");
-            book2.ShelveBook("B","3");
+            
+            Sector sectorA = new Sector("A");
+            Shelve shelve7 = new Shelve("7");
+            Sector sectorB = new Sector("B");
+            Shelve shelve3 = new Shelve("3");
+            
+            LibraryBook libraryBook1 = new LibraryBook();
+            LibraryBook libraryBook2 = new LibraryBook();
 
+            libraryBook1.ShelveBook(sectorA, shelve7);
+            libraryBook2.ShelveBook(sectorB, shelve3);
+
+            Console.WriteLine($"Book 1 - Title: {book1.Title}, Author: {book1.Author}");
+            Console.WriteLine($"Library Location - Sector: {libraryBook1.LibrarySector.Name}, Shelve: {libraryBook1.LibraryShelve.Number}");
+            Console.WriteLine($"Book 2 - Title: {book2.Title}, Author: {book2.Author}");
+            Console.WriteLine($"Library Location - Sector: {libraryBook2.LibrarySector.Name}, Shelve: {libraryBook2.LibraryShelve.Number}");
+
+            //book1.ShelveBook("A","7");
+            //book2.ShelveBook("B","3");
         }
     }
 }
